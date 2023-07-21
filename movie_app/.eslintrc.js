9765312591e06ac97571fae84092095d6fd21d73
minhthuong031103 <nominh67@gmail.com>
2023-07-21 14:07:43 +0700

    init tailwind, react navigation

--- movie_app/.eslintrc.js
+++ movie_app/.eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  'react/react-in-jsx-scope': 'off',
+  'react/jsx-uses-react': 'off',
 };
---
+++ movie_app/App.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+import AppNavigation from './navigation/AppNavigation';
+
+function App() {
+  return <AppNavigation />;
+}
+export default App;
