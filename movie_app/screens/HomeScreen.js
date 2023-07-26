b1ff5f307902b7c0fdf045bc339f605da40338dd
minhthuong031103 <nominh67@gmail.com>
2023-07-26 16:32:08 +0700

    up

--- movie_app/screens/HomeScreen.js
+++ movie_app/screens/HomeScreen.js
@@ -1,10 +1,15 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView, Platform} from 'react-native';
 import React from 'react';
 
+const ios = Platform.OS == 'ios';
 export default function HomeScreen() {
   return (
-    <View className="mt-10">
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-neutral-800">
+      <SafeAreaView className={`${ios ? '-mb-2' : 'mb-3'}`}>
+        <View>
+          <Text>Hell</Text>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
